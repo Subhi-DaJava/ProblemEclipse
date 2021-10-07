431a0ef32452bbd65e04bc994b7c7fb170365fd9
Subhi-DaJava <subhyfr809@gmail.com>
2021-10-08 01:08:43 +0200

    problème enfin résolu

--- problemEclipse2/src/problemEclipse2/ProblemExiteToujours.java
+++ problemEclipse2/src/problemEclipse2/ProblemExiteToujours.java
@@ -4,6 +4,11 @@ public class ProblemExiteToujours {
 	public static void main(String[] args) {
 		System.out.println("Comment régler ce genre de problème?");
 		System.out.println("Eclipse ne veut pas pusher notre commit, pourquoi?");
+		System.out.println("Enfin, j'ai réglé le gros problème");
+		System.out.println("Maintenat, mon Eclipse peut pusher les commits ver GitHub, Ha ha,"
+				+ "grâce à un setting de GitHube, personnel Acces token, là, "
+				+ "j'ai créé mon acces, "
+				+ "j'ai choisi uniquement le premier choix;");
 	}
 
 }
