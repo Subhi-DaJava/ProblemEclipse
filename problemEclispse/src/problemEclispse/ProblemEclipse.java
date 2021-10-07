8268e8c6beac5eab32c519ddc66f285222a4d717
Subhi-DaJava <subhyfr809@gmail.com>
2021-10-07 23:50:11 +0200

    This is the first commit for ProblemEclipse

---
+++ problemEclispse/src/problemEclispse/ProblemEclipse.java
@@ -0,0 +1,10 @@
+package problemEclispse;
+
+public class ProblemEclipse {
+
+	public static void main(String[] args) {
+		System.out.println("Pour régler le poroblème, j'ai créé ce projet.");
+
+	}
+
+}
