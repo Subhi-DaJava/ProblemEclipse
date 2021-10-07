68184572b4180dc6d772b136b801f70220e7e287
Subhi-DaJava <subhyfr809@gmail.com>
2021-10-07 23:52:45 +0200

    Create one branch, hot-fix for practicing push, pull or merge

--- problemEclispse/src/problemEclispse/ProblemEclipse.java
+++ problemEclispse/src/problemEclispse/ProblemEclipse.java
@@ -4,6 +4,7 @@ public class ProblemEclipse {
 
 	public static void main(String[] args) {
 		System.out.println("Pour régler le poroblème, j'ai créé ce projet.");
+		System.out.println("Créer une branche pour la pratique push, pull et merge etc.");
 
 	}
 
